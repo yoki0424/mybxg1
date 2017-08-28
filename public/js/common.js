f0867c7e6a1653ffa0603ff68f3cca18b1b453d1
yoki <1347186841@qq.com>
2017-08-28 11:44:36 +0800

    完成退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,19 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+	// 退出功能
+	$('#logoutBtn').click(function(){
+		console.log(123);
+		$.ajax({
+			url: '/api/logout',
+			type: 'post',
+			dataType: 'json',
+			success:function(data){
+				if(data.code == 200){
+					location.href = '/main/login';
+				}
+			}
+		})
+		
+		
+	})

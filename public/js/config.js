afaa63ef1322001daed9f0b441745d938d78af58
yoki <1347186841@qq.com>
2017-09-03 20:35:57 +0800

    完成头像上传功能

--- public/js/config.js
+++ public/js/config.js
@@ -9,6 +9,7 @@ require.config({
 		language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
 		form:'jquery-form/jquery.form',
 		validate:'validate/jquery-validate.min',
+		uploadify:'uploadify/jquery.uploadify.min',
 		common:'../js/common',
 		login:'../js/login',
 		index:'../js/index',
@@ -26,6 +27,9 @@ require.config({
 
 		},
 		validate:{
+			deps:['jquery']
+		},
+		uploadify:{
 			deps:['jquery']
 		}
 	}
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','uploadify'],function($,template,util){
 	// 设置导航菜单选中
 	util.setMenu(location.pathname);
 	// 调用后台接口填充表单
@@ -10,6 +10,21 @@ define(['jquery','template','util'],function($,template,util){
 			// 解析数据渲染页面
 			var html = template('settingsTpl',data.result);
 			$('#settingsInfo').html(html);
+			// 处理头像上传
+			$('#upfile').uploadify({
+				width:120,
+				height:120,
+				buttonText:"",
+				itemTemplate:"<span></span>",
+				swf:'/public/assets/uploadify/uploadify.swf',
+				uploader:'/api/uploader/avatar',
+				fileObjName:"tc_avatar",
+				onUploadSuccess:function(f,data){
+					var data = JSON.parse(data);
+					// 重置头像的图片地址
+					$('.preview img').attr('src',data.result.path);
+				}
+			})
 		}
 	})
 })

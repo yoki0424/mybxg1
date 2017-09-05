53f35b38a2e7b0b65a20812d37634ff40bd6bf09
yoki <1347186841@qq.com>
2017-09-06 01:57:06 +0800

    展示课程封面信息

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,17 @@
 define(['jquery','template','util'],function($,template,util){
 	// 设置导航菜单选中
 	util.setMenu('/course/add');
+	// 获取课程id
+	var csid = util.qs('cs_id');
+	// 根据课程id查询课程的封面信息
+	$.ajax({
+		type:'get',
+		url:'/api/course/picture',
+		data:{cs_id:csid},
+		dataType:'json',
+		success:function(data){
+			var html = template('pictureTpl',data.result);
+			$('#pictureInfo').html(html);
+		}
+	});
 });

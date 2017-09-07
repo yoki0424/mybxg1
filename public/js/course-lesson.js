ca2f48fe7f924702b8816a893e37b2100d1394c6
yoki <1347186841@qq.com>
2017-09-07 10:25:47 +0800

    完成编辑和添加课时的提交功能

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','bootstrap'],function($,template,util){
+define(['jquery','template','util','bootstrap','form'],function($,template,util){
 	// 设置导航菜单栏选中
 	util.setMenu('/course/list');
 	// 获取课程id
@@ -15,20 +15,52 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
 			// 渲染数据
 			var html = template('lessonTpl',data.result);
 			$('#lessonInfo').html(html);
+
+
+			// 表单提交功能
+			function submitForm(url,ctid){
+				var param = {ct_cs_id:csid};
+				if(ctid){
+					param.ct_id = ctid;
+				}
+				$('#submitBtn').click(function(){
+					$("#modalForm").ajaxSubmit({
+						type:'post',
+						url:url,
+						data:param,
+						dataType:'json',
+						success:function(data){
+							if(data.code == 200){
+								location.reload();
+							}
+						}
+
+					});
+				});
+			}
+
+
 			// 处理添加功能
 			$('#addBtn').click(function(){
+				// 调用模板
 				var html = template('modalTpl',{operate:'添加课时'});
 				$('#modalInfo').html(html);
+				// 弹出模态框
 				$('#chapterModal').modal();
+
+				// 添加提交表单
+				submitForm('/api/course/chapter/add');
 			});
 			// 处理编辑功能
 			$('.editLesson').click(function(){
 				$('#chapterModal').modal();
 				// 先查询数据
+				// 
+				var ctid = $(this).attr('data-ctid');
 				$.ajax({
 					typr:'get',
 					url:'/api/course/chapter/edit',
-					data:{ct_id:$(this).attr('data-ctid')},
+					data:{ct_id:ctid},
 					dataType:'json',
 					success:function(data){
 						data.result.operate = '编辑课时';
@@ -36,6 +68,9 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
 						$('#modalInfo').html(html);
 						// 显示弹窗
 						$('#chapterModal').modal();
+
+						// 编辑提交表单
+						submitForm('/api/course/chapter/modify',ctid);
 					}
 				})
 			});
